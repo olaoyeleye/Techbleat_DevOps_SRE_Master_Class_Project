3156cb186246c1376b6792cf971ddd3b46a842c8
Olu <olaoyeleye@yahoo.co.uk>
2025-12-31 09:46:01 +0000

    added backend for statefile

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "techbleat-terraform-state-CynWumOye"
+    bucket = "techbleat-terraform-state-cynwumoye"
     key    = "CynWumOye/terraform.tfstate"
     region = "eu-west-1"
     encrypt = true
